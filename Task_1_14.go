3e1071dfd489df46dd820bb1172653cce347a1f8
Vladislav <vladislav19991@gmail.com>
2025-08-18 23:01:18 +0300

    idk

--- Task_1_14.go
+++ Task_1_14.go
@@ -3,23 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var n, d, counter, sum, number, ten int
+	var n, d int
 
 	fmt.Scan(&n, &d)
 
-	for n > 0 {
-		sum = n % 10
-		n /= 10
-		if sum != d {
-			ten = 1
-
-			for i := 0; i < counter; i++ {
-				ten *= 10
-			}
-
-			number += sum * ten
-		}
-	}
-
-	fmt.Println(number)
+	fmt.Printf("%b", n)
 }
